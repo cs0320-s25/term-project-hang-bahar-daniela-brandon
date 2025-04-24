aee44d0314444a8129ff20ef1fb40cc605aee28f
hangnguyen <hangnguyen.user@gmail.com>
2025-04-23 21:21:47 -0400

    feat: draft implementation of search algorithm with mock dorm datasource

---
+++ server/src/main/java/org/example/DormDataSourceFactory.java
@@ -0,0 +1,22 @@
+package org.example;
+
+// DormDataSourceFactory.java
+public class DormDataSourceFactory {
+  public enum DataSourceType {
+    MOCK,
+    FIREBASE
+  }
+
+  public static DormDataSource createDataSource(DataSourceType type) {
+    switch (type) {
+      case MOCK:
+        return new MockDormDataSource();
+      case FIREBASE:
+        // This will be implemented later
+        // return new FirebaseDormDataSource();
+        throw new UnsupportedOperationException("Firebase implementation not ready yet");
+      default:
+        throw new IllegalArgumentException("Unknown data source type");
+    }
+  }
+}

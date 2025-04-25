840d0bcccfc4dd1d70f29baf8782d4f3d53b044b
bsun786 <brandon_sun1@brown.edu>
2025-04-25 18:21:38 -0400

    Revert "front end framework written"
    This reverts commit 8f7abb52e4d6955979ee3147efcf8f3275baed7f.

--- client/app/routes.ts
+++
@@ -1,8 +0,0 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
-
-export default [
-  index("routes/home.tsx"),
-  route("/reviews", "routes/reviews.tsx"),
-  route("/make-post", "routes/makePost.tsx"),
-  route("/recommend", "routes/recommend.tsx"),
-] satisfies RouteConfig;

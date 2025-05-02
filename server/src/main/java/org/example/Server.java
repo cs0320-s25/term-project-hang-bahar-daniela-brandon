5661162611470078c3f49321f26b38eab5804f85
hangnguyen <hangnguyen.user@gmail.com>
2025-05-02 12:12:17 -0400

    fix: removed print logs

--- server/src/main/java/org/example/Server.java
+++ server/src/main/java/org/example/Server.java
@@ -3,7 +3,6 @@ package org.example;
 import static spark.Spark.after;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import org.example.Handlers.GetDormsHandler;
@@ -18,37 +17,27 @@ public class Server {
     System.out.println("Starting server...");
     int port = 5678;
     Spark.port(port);
-    System.out.println("555");
     after(
         (Filter)
             (request, response) -> {
               response.header("Access-Control-Allow-Origin", "*");
               response.header("Access-Control-Allow-Methods", "*");
             });
-    System.out.println("666");
 
     DormDataSource dataSource = DormDataSourceFactory.createDataSource(DormDataSourceFactory.DataSourceType.MOCK);
-    System.out.println("777");
     // fetches the data at http://localhost:3232/search?query=quiet Or /info?query=getAllDorms
     Spark.get("/search", new SearchHandler(dataSource));
     Spark.get("/info", new GetDormsHandler(dataSource));
-    Spark.get("/", (req, res) -> "Hello World");
-    Spark.get("/test", (req, res) -> "Test successful");
-    System.out.println("888");
 
     Spark.notFound(
         (request, response) -> {
           response.status(404); // Not Found
           System.err.println("ERROR");
-          System.out.flush();
           return "404 Not Found - The requested endpoint does not exist.";
         });
 
-    System.out.println("999");
     Spark.init();
-    System.out.println("aaa");
     Spark.awaitInitialization();
-    System.out.println("bbb");
 
     System.out.println("Server started at http://localhost:" + port);
   }

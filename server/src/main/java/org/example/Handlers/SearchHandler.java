b973d2e8cb57fe3d2ee4e3bc552bb3550292e740
bahar-codes <144497582+bahar-codes@users.noreply.github.com>
2025-05-14 18:11:09 +0200

    @docs: added documentation

--- server/src/main/java/org/example/Handlers/SearchHandler.java
+++ server/src/main/java/org/example/Handlers/SearchHandler.java
@@ -11,13 +11,42 @@ import spark.Response;
 import spark.Route;
 import com.google.gson.Gson;
 
+/**
+ * Handles HTTP GET requests to search for dormitories based on a query parameter.
+ * This class implements the Spark Route interface, allowing it to be
+ * used as a route handler within a Spark Java web application. When a GET
+ * request is made to the associated endpoint with a 'query' parameter, this
+ * handler processes the input and returns a list of matching dormitories in
+ * JSON format.
+ * If the 'query' parameter is missing or empty, the handler responds with a
+ * 400 Bad Request status and an error message. If no matching dormitories are
+ * found, it responds with a 404 Not Found status and an appropriate message.
+ */
+
+
 public class SearchHandler implements Route {
   private DormDataSource dataSource;
   private Gson gson = new Gson();
-
+	
+	/**
+	* Constructs a new SearchHandler with the specified data source.
+	*
+	* @param dataSource the data source used to retrieve and search dormitory data
+	*/
   public SearchHandler(DormDataSource dataSource) {
     this.dataSource = dataSource;
   }
+
+    /**
+     * Handles the HTTP GET request by retrieving the 'query' parameter, searching
+     * for matching dormitories based on this query, and returning the results as
+     * a JSON-formatted string.
+     *
+     * @param req the HTTP request containing the 'query' parameter
+     * @param res the HTTP response
+     * @return a JSON-formatted string containing matching dormitories or an error message
+     * @throws Exception if an error occurs while processing the request
+     */
 
   @Override
   public Object handle(Request req, Response res) throws Exception {
@@ -26,7 +55,6 @@ public class SearchHandler implements Route {
       res.status(400);
       return "Missing query parameter";
     }
-
 	List<DormSearchResult> dorms = dataSource.searchDorms(query);
 
 	if (dorms.isEmpty()) {
@@ -36,7 +64,5 @@ public class SearchHandler implements Route {
 	else {
 		return gson.toJson(dataSource.searchDorms(query));
 	}
-
-    
   }
 }

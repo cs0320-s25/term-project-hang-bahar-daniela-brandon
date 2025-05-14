a1d32d6331390fc013e80f1eda2d6dd7cea3cd00
bahar-codes <144497582+bahar-codes@users.noreply.github.com>
2025-05-14 16:28:44 +0200

    @docs: added documentation

--- server/src/main/java/org/example/Dorms/DormDataSource.java
+++ server/src/main/java/org/example/Dorms/DormDataSource.java
@@ -3,6 +3,10 @@ package org.example.Dorms;
 import com.google.gson.JsonObject;
 import java.util.List;
 
+/**
+ * This is an interface for dorm data source and is implemented by MockDormDatasource and FirebseDormDatasource
+ * It has the shared funcionalities of those classes such as search or getinfo through the database with the provided query
+ */
 public interface DormDataSource {
   List<Dorm> getAllDorms();
 

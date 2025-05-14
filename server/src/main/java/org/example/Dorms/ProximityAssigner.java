33019b3b32d833ff501675585ac5c2d10f532341
bahar-codes <144497582+bahar-codes@users.noreply.github.com>
2025-05-14 17:59:46 +0200

    @docs: added documentation

--- server/src/main/java/org/example/Dorms/ProximityAssigner.java
+++ server/src/main/java/org/example/Dorms/ProximityAssigner.java
@@ -6,6 +6,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * This class is a proximity assigner. It maps list of dorms to a certain location on campus such as Main Green
+ * 
+ */
+
 public class ProximityAssigner {
 
   private static final Map<String, List<String>> PROXIMITY_MAP = new HashMap<>();
@@ -16,6 +21,13 @@ public class ProximityAssigner {
     PROXIMITY_MAP.put("North Campus", List.of("sternlichtcommons", "mindenhall", "machadohouse"));
     PROXIMITY_MAP.put("South Campus", List.of("barbourhall", "youngorchard2", "chenfamilyhall", "gradcentera", "gradcenterb", "gradcenterc", "gradcenterd", "gregorianquada", "gregorianquadb", "youngorchard4", "youngorchard5", "youngorchard10", "danoffhall", "kinghouse", "perkinshall"));
   }
+
+  
+  /**
+   * this helper method returns proximity of the specified dorm from the list of "benchmark points" specified above
+   * @param: name of the dorm of interest
+   * @return: proximity from the specified dorm
+   */
 
   public static Set<String> getProximity(String dormName) {
     Set<String> proximities = new HashSet<>();
